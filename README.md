82dbfc8776b740c5626561750702b504b15f5dd0
Edward <edward.lin20098@gmail.com>
2026-09-11 20:21:24 -0400

    Deploy website

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # EdwardKairuiLinWebsite
+
+Personal portfolio website built with React and Vite.
+
+## Local development
+
+npm install
+npm run dev
+
+## Production build
+
+npm run build
+
+## Deploy
+
+npm run deploy
+
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster";
+
+function App() {
+  return (
+    <>
+      <Toaster />
+      <HashRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;
